b193c92ee19f881f1be4694b589af17a3efd00c0
Cleiton Gomes <cleiton.gomes@abecom.local>
2020-08-26 17:23:38 -0300

    adicionado comentarios nas etapas do codigo

--- desafioMod4/desafioJavascritpt0402.js
+++ desafioMod4/desafioJavascritpt0402.js
@@ -17,60 +17,77 @@ Depois de preencher o input e adicionar, a seguinte lista deve aparecer abaixo:
 </ul>
 
  */
-
+ 
+ //função que busca os repositorios de um usuario no github
 function searchUserInGithub(){  
+    //previne a pagina de dar reload após click do botão
     event.preventDefault()
-
-    
-
+    //captura o valor contido no input
     const username = document.querySelector('#username').value
-    
-
+    //referencia a div de id repositorys 
     const listRepo = document.querySelector('#respositorys')
+    //remove qualquer valor dentro da div repositorys
     listRepo.innerHTML = '';
     
+    //solicitação ajax com axios para a api do github
     axios.get(`https://api.github.com/users/${username}/repos`)
-    
+    //em caso de sucesso, aciona a função .then, que recebe uma função usando a resposta como parametro
     .then(function(response){
+        //exibe no terminal o retorno da informação obtida na requisição do axios
         console.log(response.data)
+        //invoca a função limparCampo, para zerar o valor dentro do input
         limparCampo(username)
-
-            if (response.data.length == 0){
-               const noReposit = document.createElement('h1')
-                noReposit.textContent = 'Usuário não possui repositorios'
-                listRepo.appendChild(noReposit)
+            
+        //valida se o tamanho de data da resposta obtida é igual a 0
+        if (response.data.length == 0){
+            //cria elemento h1
+            const noReposit = document.createElement('h1')
+            //especifica o texto contido em h1
+            noReposit.textContent = 'Usuário não possui repositorios'
+            //anexa esse h1 como elemento filho da div repositorys
+            listRepo.appendChild(noReposit)
+            //caso o data da resposta da requisição maior que 0
             } else {
+                //para cada elemento dentro de data que é um nodeList
+                for (repo of response.data) {
+                //cria um li     
+                const listReposit = document.createElement('li')
+                //adiciona uma classe css a essa li criada
+                listReposit.classList.add('repose')
 
-            for (repo of response.data) {
-                
-            const listReposit = document.createElement('li')
-            listReposit.classList.add('repose')
-            
-            const link = document.createElement('a')
-            link.setAttribute('href', repo.html_url)
-            link.setAttribute('target','_blank')
-            link.textContent = repo.name
-            
-            listReposit.appendChild(link)
-            listRepo.appendChild(listReposit)
-         } 
+                //cria um a (link)
+                const link = document.createElement('a')
+                //adiciona o atributo href onde o seu valor é o conteudo é o link do github para aquele repositorio
+                link.setAttribute('href', repo.html_url)
+                //adiciona o atributo target com o valor _blank para quando o link for aberto, abrir em nova aba
+                link.setAttribute('target','_blank')
+                //adiciona o texto a tag a com o nome do repositorio obtido via a requisição ajax
+                link.textContent = repo.name
+                //anexa o elemento 'a' como elemento filho do elemento 'li'
+                listReposit.appendChild(link)
+                //anexa o elemento 'li' como elemento filho do elemento div repopsitorys
+                listRepo.appendChild(listReposit)
+                } 
         
-        }
-    } 
+            }
+        }    
     )
+    //em caso de erro da requisição do axios, sera acessado a função catch, com a mensagem de erro obtida
     .catch(function(error){
+        //exibe a mensagem de erro no terminal
         console.log(error)
-        const listRepo = document.querySelector('#respositorys')
-        listRepo.innerHTML = '';
+        //cria um elemento h1
         const msg_error = document.createElement('h1')
+        //anexa um texto a ser usado por esse h1
         msg_error.textContent = 'Não foi possível localizar esse usuário'
-
+        //anexa h1 como elemento filho da div repositorys
         listRepo.appendChild(msg_error)
     })
     
 }
 
-
+//função para limpar o campo input
 function limparCampo(){
+    //define o valor do input como '' (vazio)
     document.querySelector('#username').value = '';
 }
